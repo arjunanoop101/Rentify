50ec67aa86e6db8d12db30f061dd0bec97cc1130
arunbm-dev <arunnerody@gmail.com>
2024-05-06 22:15:45 +0530

    added order page

--- src/pages/MyOrders/MyOrders.jsx
+++ src/pages/MyOrders/MyOrders.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React,{useContext}from 'react'
 import './MyOrders.css'
+import {StoreContext} from '../../context/StoreContext'
 import Sidebar from '../../components/Sidebar/Sidebar'
+import { assets } from '../../assets/assets'
+import orderpng from '../../assets/order.png'
 const MyOrders = () => {
+
+  const{productList}=useContext(StoreContext)
+
   return (
     <div className='my-orders'>
     <Sidebar/>
-    MyOrders
+    <div className="order">
+      <div>
+        <h4>Order Page</h4>
+      </div>
+      <div className="order-list">
+      {productList.map((item,index)=>{
+            return(
+              <div>
+                <div className="order-image">
+                  <img src={orderpng} alt="" />
+                </div>
+                <div key={index} className='list-items'>
+                  <p>{item.productName}</p>
+                </div>
+              </div>  
+            )
+          })}
+      </div>
+    </div>
     </div>
   )
 }
